5f1bb2ea61e904e478fb549bd4784927867c1465
Matt Vanderpol <matt@vanderpol.net>
2016-11-14 08:34:14 -0800

    Basic config for JS linting

---
+++ eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+      "browser": true,
+      "node": true
+    },
+    "settings": {
+      "ecmascript": 5
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+    }
+};
